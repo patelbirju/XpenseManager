05dee96a9216f10fecd19c6ee69db67a152349f1
patelbirju <birjupatel95@hotmail.com>
2017-10-23 12:12:41 -0400

    Added user table with SQLite (insert working)

--- app/src/main/java/com/example/birju_000/xpensemanager/signup.java
+++ app/src/main/java/com/example/birju_000/xpensemanager/signup.java
@@ -1,8 +1,10 @@
 package com.example.birju_000.xpensemanager;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -17,10 +19,19 @@ public class signup extends AppCompatActivity
 
     private Button createAccountBtn;
 
+    private static String firstName;
+    private static String lastName;
+    private static String email;
+    private static String password;
+
+    private UserDB.UserDbHelper helper;
+    private SQLiteDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
 
         createAccountBtn = (Button) findViewById(R.id.createAccountBtn);
         createAccountBtn.setOnClickListener(this);
@@ -32,11 +43,47 @@ public class signup extends AppCompatActivity
         //
         //
         //Display message to the user
-        Toast.makeText(this, "Account Successfully Created.", Toast.LENGTH_LONG).show();
+        firstName = ((EditText) findViewById(R.id.firstNameText)).getText().toString();
+        lastName = ((EditText) findViewById(R.id.lastNameText)).getText().toString();
+        email = ((EditText) findViewById(R.id.emailText)).getText().toString();
+        password = ((EditText) findViewById(R.id.passwordTextSignUp)).getText().toString();
+
+
+        try{
+            ContentValues values = new ContentValues();
+            values.put(UserDB.FIRST_NAME, firstName);
+            values.put(UserDB.LAST_NAME, lastName);
+            values.put(UserDB.EMAIL, email);
+            values.put(UserDB.PASSWORD, password);
+
+            UserDB user = new UserDB(getApplicationContext());
+
+            long rowID = user.insertUser(values);
+            if(rowID > 0){
+                Toast.makeText(this, "Account Successfully Created. "+rowID, Toast.LENGTH_LONG).show();
+            }
+
+
+        }
+        catch (Exception ex){
+            Log.e("Exception message: ",ex.getMessage());
+        }
+
+
     }
 
     @Override
     public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
         return false;
+    }
+
+    public static ContentValues getUserData(){
+        ContentValues values = new ContentValues();
+        values.put(UserDB.FIRST_NAME, firstName);
+        values.put(UserDB.LAST_NAME, lastName);
+        values.put(UserDB.EMAIL, email);
+        values.put(UserDB.PASSWORD, password);
+
+        return values;
     }
 }
